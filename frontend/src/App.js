1401d7073d636b0309d8cbd34e92dd2835217546
Junhyunny <50021245+Junhyunny@users.noreply.github.com>
2022-09-18 00:32:35 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload. 
         </p>
         <p>
-          Hello World.
+          Hello Junhyunny's Dev World.
         </p>
         <a
           className="App-link"
